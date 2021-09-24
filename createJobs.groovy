c2faa5d3acc3b028605e4e985cce68d1940f275b
tobias2022 <89058974+tobias2022@users.noreply.github.com>
2021-09-25 01:05:39 +1000

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/tobias2022/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
@@ -26,7 +26,7 @@ pipelineJob('theme-park-job-docker') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/tobias2022/spring-boot-api-example.git'
                     }
                     branch 'master'
                     scriptPath('Jenkinsfile-docker')
@@ -41,7 +41,7 @@ pipelineJob('theme-park-job-aws') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/tobias2022/spring-boot-api-example.git'
                     }
                     branch 'master'
                     scriptPath('Jenkinsfile-aws')
